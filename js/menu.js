4cabb6855cffa1dcd6ebbb283007a7985ffc598c
Jimmy Luna <jimmylunad@gmail.com>
2020-12-16 02:09:17 -0500

    cambios en mobile

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,20 @@
 // selector
 var menu = document.querySelector('.hamburger');
 
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+
+menuBtn.addEventListener('click', () => {
+	if(!menuOpen) {
+		menuBtn.classList.add('open');
+		menuOpen = true;
+	} else {
+		menuBtn.classList.remove('open');
+		menuOpen = false;
+	}
+});
+
+
 // method
 function toggleMenu (event) {
   this.classList.toggle('is-active');
